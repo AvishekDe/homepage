8003859755fdc4a7d9efe7c9b1ef7d2c8bc040e9
AvishekDe <avishekde.iitr@gmail.com>
2014-05-14 10:51:34 +0530

    * Completed app page and frontend for feedback page * Introduced self-customized tooltip.css instead of hint.css

---
+++ js/sliders.js
@@ -0,0 +1,11 @@
+function updateSliderUX(slideAmount) {
+	$("#chosenUX").html(slideAmount);
+}
+
+function updateSliderAP(slideAmount) {
+	$("#chosenAP").html(slideAmount);
+}
+
+function updateSliderUS(slideAmount) {
+	$("#chosenUS").html(slideAmount);
+}
