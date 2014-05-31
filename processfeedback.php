3d7757cd8be415a798e476fec55e14ef665ab442
AvishekDe <avishekde.iitr@gmail.com>
2014-05-31 11:29:37 +0530

    Established connection of feedback page to database

---
+++ processfeedback.php
@@ -0,0 +1,14 @@
+<?php
+	$con = mysql_connect("localhost", "root", "drinkdeep");
+	$db = mysql_select_db("homepage", $con);
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$feedb = $_POST['feedback'];
+	$ux = $_POST['slideUX'];
+	$app = $_POST['slideAP'];
+	$us = $_POST['slideUS'];
+
+	$query = mysql_query("insert into feedback (Name , Email , Feedback , Userx , Appearance, Usability) values ('$name' , '$email' , '$feedb' , '$ux' , '$app' , '$us')");
+	mysql_close ("$con");
+?>
