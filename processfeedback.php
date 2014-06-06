48b94ee40eaca04aab3e8c5043d18ce9e210dafa
AvishekDe <avishekde.iitr@gmail.com>
2014-06-06 13:37:10 +0530

    Added validation for forms

--- processfeedback.php
+++ processfeedback.php
@@ -1,13 +1,30 @@
 <?php
+
+// define variables and set to empty values
+$name = $email = $feedb = $ux = $app = $us = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $feedb = test_input($_POST["feedback"]);
+  $ux = test_input($_POST["slideUX"]);
+  $app = test_input($_POST["slideAP"]);
+  $us = test_input($_POST['slideUS']);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+
+
+
 	$con = mysql_connect("localhost", "root", "drinkdeep");
 	$db = mysql_select_db("homepage", $con);
-
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$feedb = $_POST['feedback'];
-	$ux = $_POST['slideUX'];
-	$app = $_POST['slideAP'];
-	$us = $_POST['slideUS'];
 
 	$query = mysql_query("insert into feedback (Name , Email , Feedback , Userx , Appearance, Usability) values ('$name' , '$email' , '$feedb' , '$ux' , '$app' , '$us')");
 	mysql_close ("$con");
--- processphone.php
+++ processphone.php
@@ -1,10 +1,26 @@
 <?php
+
+
+	$name = $mobile = $reason = "";
+
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  $name = test_input($_POST["name"]);
+	  $mobile = test_input($_POST["mobile"]);
+	  $reason = test_input($_POST["reason"]);
+	}
+
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+
+
+
 	$con = mysql_connect("localhost", "root", "drinkdeep");
 	$db = mysql_select_db("homepage", $con);
-
-	$name = $_POST['name'];
-	$mobile = $_POST['mobile'];
-	$reason = $_POST['reason'];
 
 	$query = mysql_query("insert into contactrequest (name , reason , contactno) values ('$name' , '$reason' , '$mobile')");
 	mysql_close ("$con");
